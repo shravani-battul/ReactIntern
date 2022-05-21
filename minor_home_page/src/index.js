ef8314efd479c11cc8074192d655b02a2a08310f
shravani-battul <shravani.battul@outlook.com>
2022-05-21 08:25:00 +0000

    added css ,logo and home page

--- minor_home_page/src/index.js
+++ minor_home_page/src/index.js
@@ -1,17 +1,126 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from 'react-dom'
+import logo from './fashio.gif'
+import css from './styles.css'
+ReactDOM.render(
+<>
+  <html>
+  
+  <head>
+    <title>
+      React Minor Project
+    </title>
+  </head>
+  
+  <body>
+    <nav class="navbar background">
+      <ul class="nav-list">
+        <div class="logo">
+          <img src={logo}/>
+        </div>
+        <li><a href="#home">HOME</a></li>
+        <li><a href="#about_us">ABOUT US</a></li>
+        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#products">PRODUCTS</a></li>
+        <li><a href="#contact_us">CONTACT US</a></li>
+      </ul>
+  
+      <div class="rightNav">
+         
+        <button class="btn btn-sm">Search</button>
+      </div>
+    </nav>
+  
+    <section class="firstsection">
+      <div class="box-main">
+        <div class="firstHalf">
+          <h1 class="text-big" id="web">
+            Web Technology
+          </h1>
+          
+          <p class="text-small">
+            HTML stands for HyperText Markup
+            Language. It is used to design
+            web pages using a markup language.
+            HTML is the combination of Hypertext
+            and Markup language. Hypertext
+            defines the link between the web
+            pages. A markup language is used
+            to define the text document within
+            tag which defines the structure of
+            web pages. HTML is a markup language
+            that is used by the browser to
+            manipulate text, images, and other
+            content to display it in the required
+            format.
+          </p>
+  
+  
+        </div>
+      </div>
+    </section>
+  
+    <section class="secondsection">
+      <div class="box-main">
+        <div class="secondHalf">
+          <h1 class="text-big" id="program">
+            C Programming
+          </h1>
+          <p class="text-small">
+            C is a procedural programming language.
+            It was initially developed by Dennis
+            Ritchie as a system programming
+            language to write operating system.
+            The main features of C language include
+            low-level access to memory, simple set
+            of keywords, and clean style, these
+            features make C language suitable for
+            system programming like operating system
+            or compiler development.
+          </p>
+  
+  
+        </div>
+      </div>
+    </section>
+  
+    <section class="section">
+      <div class="paras">
+        <h1 class="sectionTag text-big">Java</h1>
+  
+        <p class="sectionSubTag text-small">
+          Java has been one of the most
+          popular programming language
+          for many years. Java is Object
+          Oriented. However it is not
+          considered as pure object oriented
+          as it provides support for primitive
+          data types (like int, char, etc) The
+          Java codes are first compiled into byte
+          code (machine independent code). Then
+          the byte code is run on Java Virtual
+          Machine (JVM) regardless of the
+          underlying architecture.
+        </p>
+  
+  
+      </div>
+  
+      <div class="thumbnail">
+        <img src={logo}/>
+      </div>
+    </section>
+  
+    <footer class="background">
+      <p class="text-footer">
+        Copyright ©-All rights are reserved
+      </p>
+  
+  
+    </footer>
+  </body>
+  
+  </html>
+</>,
+document.getElementById("root")
+)
